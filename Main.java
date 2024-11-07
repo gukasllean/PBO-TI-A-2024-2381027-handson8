742a436911d1fa0399277ec5977989cc5479587a
Lukas <2381027@unai.edu>
2024-11-07 10:10:20 +0700

    handson8

---
+++ Main.java
@@ -0,0 +1,17 @@
+import repositories.TodoListRepository;
+import repositories.TodoListRepositoryImpl;
+import services.TodoListService;
+import services.TodoListServiceImpl;
+import views.TodoListTerminalView;
+import views.TodoListView;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListTerminalView(todoListService);
+        todoListView.run();
+    }
+}
