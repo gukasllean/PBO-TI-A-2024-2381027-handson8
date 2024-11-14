6745cfd50fbf773f20fafd88f66e279f557fe046
Lukas <2381027@unai.edu>
2024-11-14 10:19:31 +0700

    handson9

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
+import config.Database;
 import repositories.TodoListRepository;
+import repositories.TodoListRepositoryDblmpl;
 import repositories.TodoListRepositoryImpl;
 import services.TodoListService;
 import services.TodoListServiceImpl;
 import views.TodoListTerminalView;
 import views.TodoListView;
 
+import javax.xml.crypto.Data;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        Database database = new Database("my_database", "root","","localhost","3306");
+        database.setup();
+
+
+        TodoListRepositoryDblmpl todoListRepository = new TodoListRepositoryDblmpl(database);
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
         TodoListView todoListView = new TodoListTerminalView(todoListService);
         todoListView.run();
